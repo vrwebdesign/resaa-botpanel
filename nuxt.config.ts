b2802efdd855e96c2e49c76bc24c2ae9bfd7e7e7
armin <arminkheirkhahan@gmail.com>
2020-04-18 12:18:03 +0430

    fix host

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,8 +16,7 @@ require('events').EventEmitter.defaultMaxListeners = 50
 export default {
   mode: 'spa',
   server: {
-    port: process.env.PORT,
-    host: 'resaa.dev'
+    port: process.env.PORT
   },
   axios: {
     proxy: true, // Can be also an object with default options

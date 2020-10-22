fb291c2f0ea05e2a3998b97cba1fa8a34bd1057d
armin <arminkheirkhahan@gmail.com>
2020-10-22 16:23:48 +0330

    add new status

--- enums/corona_transaction_status.ts
+++ enums/corona_transaction_status.ts
@@ -6,7 +6,8 @@ export interface coronaTestPaymentStatus {
 const coronaTestPaymentStatus = {
   unpaid: 'پرداخت نشده',
   paid: 'پرداخت شده',
-  returned: 'بازگشت داده شده'
+  negotiated:'مذاکره شده',
+  returned: 'بازگشت داده شده',
 }
 export default coronaTestPaymentStatus
 
